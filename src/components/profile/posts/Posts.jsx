b5b114e27d8e501a130e7d8a31219cc50b1241b9
blacksempai <benjaminfranclin1998@gmail.com>
2022-04-23 16:22:44 +0300

    m

--- src/components/profile/posts/Posts.jsx
+++ src/components/profile/posts/Posts.jsx
@@ -1,6 +1,11 @@
 import classes from './Posts.module.css'
 import Post from './post/Post'
 
+
+const addPost = () => {
+    alert("Post Added")
+}
+
 const Posts = (props) => {
     let posts = props.posts.map((p)=>{
         return <Post text={p.text}/>
@@ -10,7 +15,7 @@ const Posts = (props) => {
            <h1>Posts</h1> 
            <form>
                 <textarea className={classes.new_post}></textarea>
-                <input className={classes.submit} type="submit" value="Send"/>
+                <input className={classes.submit} type="submit" value="Send" onClick={addPost} />
            </form>
            <div className={classes.posts}>
                 {posts}
