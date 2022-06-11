8d4fb0af6607df11603ded304797c4619766d8b9
blacksempai <benjaminfranclin1998@gmail.com>
2022-06-11 17:32:46 +0300

    add back-end

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const config = require('config');
+const PORT = config.get('port') || 5000;
+const app = express();
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoURL'));
+        app.listen(PORT, ()=> { console.log(` App has been started on port ${PORT}... `) });
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+start();
+
+app.get('/users', async (req, res) => {
+    const users = await User.find();
+    res.json(users);
+})
